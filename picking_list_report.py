e282e36d87ddd8d71e55d22b9cf2b516cee4438a
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-07-09 11:03:31 +0530

    This code is maintained by fulfil.io inc

--- picking_list_report.py
+++ picking_list_report.py
@@ -2,8 +2,6 @@
 """
     picking_list_report.py
 
-    :copyright: (c) 2015 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 
 from trytond.pool import PoolMeta, Pool
